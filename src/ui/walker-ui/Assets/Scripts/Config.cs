9537a3ae22ac0edfb12300f7a310d2aabc39ed06
Etienne Landuré <25793763+etiennelndr@users.noreply.github.com>
2023-08-28 18:45:37 +0200

    Add a new script to manage global configuration. Fix bug where walker was colliding each linecast.

--- src/ui/walker-ui/Assets/Scripts/Config.cs
+++ src/ui/walker-ui/Assets/Scripts/Config.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Config : MonoBehaviour {
     [SerializeField]
-    private int frameRate = 60;
+    private int frameRate = 10;
+
+    [SerializeField]
+    private int vSyncCount = 1;
 
     // Start is called before the first frame update
     void Start() {
         Application.targetFrameRate = frameRate;
+        QualitySettings.vSyncCount = vSyncCount;
     }
 }
--- src/ui/walker-ui/Assets/Scripts/Patrol.cs
+++ src/ui/walker-ui/Assets/Scripts/Patrol.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEngine.GraphicsBuffer;
 
 public class Patrol : MonoBehaviour {
     private NavMeshAgent agent;
@@ -18,10 +14,17 @@ public class Patrol : MonoBehaviour {
     private Transform[] targetPoints;
 
     [SerializeField]
-    private bool drawTargetPoints = false;
+    private bool targetPointsVisible = false;
+
+    [SerializeField]
+    private float targetPointsHeight = 2.5f;
+
+    [SerializeField]
+    private string apiURL;
 
     private Vector3 currentTargetPoint;
     private Vector3 previousTargetPoint;
+    private List<Vector3> nextTargetPoints;
 
     private readonly System.Random rnd = new System.Random();
 
@@ -43,7 +46,7 @@ public class Patrol : MonoBehaviour {
         if (!agent.pathPending && agent.remainingDistance < remainingDistanceTolerance)
             UpdateTarget();
 
-        if (drawTargetPoints)
+        if (targetPointsVisible)
             DrawTargetPoints();
     }
 
@@ -59,58 +62,72 @@ public class Patrol : MonoBehaviour {
         // Save previous and new target points for next update
         previousTargetPoint = currentTargetPoint;
         currentTargetPoint = newTargetPoint;
+        // Retrieve next target points for debugging purposes
+        nextTargetPoints = GetVisiblePoints(currentTargetPoint);
     }
 
     Vector3 GetNextTargetPoint() {
-        // Returns the first point if no target is set (i.e. point selected at startup)
-        if (currentTargetPoint == null) {
+        List<Vector3> nextPoints;
+        // Returns the first point if no target is currently set (i.e. point selected at startup)
+        if (currentTargetPoint == Vector3.zero) {
             Debug.Log("No target point set, defaulting to the first point available");
-            return targetPoints[0].position;
-        }
-
-        // Retrieve each point with no collider intersecting a line between it and the current target
-        List<Vector3> nextPoints = new List<Vector3>();
-        foreach (var point in targetPoints) {
-            var pointPosition = point.position;
-            // Always exclude the previous and current target points
-            if (
-                pointPosition == currentTargetPoint
-                || pointPosition == previousTargetPoint
-                || Physics.Linecast(currentTargetPoint, pointPosition)
-            ) {
-                continue;
-            }
-            nextPoints.Add(pointPosition);
-        }
-
-        // When no points are "visible", select a random one in the global list of points
-        if (nextPoints.Count == 0) {
-            Debug.LogError("Unable to find any visible target point(s), defaulting to a random one");
-            nextPoints = targetPoints
-                // Exclude the previous and current target points
-                .Where(p => p.position != currentTargetPoint && p.position != previousTargetPoint)
-                .Select(p => p.position)
-                .ToList();
+            nextPoints = GetVisiblePoints(agent.transform.position);
         }
         else {
-            Debug.Log("Found " + nextPoints.Count + " new target point(s)");
+            // Retrieve each point with no collider intersecting a line between it and the current target
+            nextPoints = GetVisiblePoints(currentTargetPoint);
+
+            // When no points are "visible", select a random one in the global list of points
+            if (nextPoints.Count == 0) {
+                Debug.LogError("Unable to find any visible target point(s), defaulting to a random one");
+                nextPoints = targetPoints.Select(p => p.position).ToList();
+            }
+            else {
+                Debug.Log("Found " + nextPoints.Count + " new target point(s)");
+            }
+
+            // Exclude the previous and current target points
+            nextPoints = nextPoints.Where(p => p != currentTargetPoint && p != previousTargetPoint).ToList();
         }
 
         int nextPointIndex = rnd.Next(nextPoints.Count);
         return nextPoints[nextPointIndex];
     }
 
+    List<Vector3> GetVisiblePoints(Vector3 point) {
+        List<Vector3> visiblePoints = new List<Vector3>();
+        foreach (var targetPoint in targetPoints) {
+            var targetPointPosition = targetPoint.position;
+            // If the current target point is the input point,
+            if (targetPointPosition == point
+                // or the point is hidden by an object,
+                || Physics.Linecast(targetPointPosition, point)
+                // or the distance between the two points is less than the required tolerance
+                || Vector3.Distance(targetPointPosition, point) < remainingDistanceTolerance
+            ) {
+                // then, ignore this point
+                continue;
+            }
+            visiblePoints.Add(targetPointPosition);
+        }
+        return visiblePoints;
+    }
+
     void DrawTargetPoints() {
         foreach (var targetPoint in targetPoints) {
             var targetPointPosition = targetPoint.position;
+
             if (targetPointPosition == currentTargetPoint) {
-                Debug.DrawRay(targetPointPosition, transform.up * 2.5f, Color.green);
+                Debug.DrawRay(targetPointPosition, transform.up * targetPointsHeight, Color.green);
             }
             else if (targetPointPosition == previousTargetPoint) {
-                Debug.DrawRay(targetPointPosition, transform.up * 2.5f, Color.blue);
+                Debug.DrawRay(targetPointPosition, transform.up * targetPointsHeight, Color.blue);
+            }
+            else if (nextTargetPoints.Contains(targetPointPosition)) {
+                Debug.DrawRay(targetPointPosition, transform.up * targetPointsHeight, Color.yellow);
             }
             else {
-                Debug.DrawRay(targetPointPosition, transform.up * 2.5f, Color.black);
+                Debug.DrawRay(targetPointPosition, transform.up * targetPointsHeight, Color.black);
             }
         }
     }
